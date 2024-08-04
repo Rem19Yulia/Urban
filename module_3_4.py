642f37cecd46ca7eb3e25bf4f0159d4055927246
Rem19Yulia <kyv190591@gmail.com>
2024-08-04 23:42:10 +0300

    Create module_3_4.py

---
+++ module_3_4.py
@@ -0,0 +1,13 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+
+    for word in other_words:
+     if root_word.lower() in word.lower() or word.lower() in root_word.lower():
+        same_words.append(word)
+
+    return same_words
+
+root = "able"
+words = ["Disablement", "Able", "able", "AbLe", "capability"]
+result = single_root_words(root, words)
+print(result)
