4b6e7aee49917fccef4d462d24453772f7f6fb52
Rem19Yulia <kyv190591@gmail.com>
2024-08-11 21:14:29 +0300

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -1,14 +1,15 @@
 class House:
     houses_history = []
 
+    def __new__(cls, *args, **kwargs):
+        name = args[0]  # предполагаем, что первый аргумент - это название
+        instance = super().__new__(cls)
+        cls.houses_history.append(name)
+        return instance
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.__class__.new(name)
-    
-    @classmethod
-    def new(cls, *args):
-        cls.houses_history.append(args[0])
 
     def __del__(self):
         print(f"{self.name} снесён, но он останется в истории")
@@ -19,10 +20,53 @@ class House:
                 print(floor)
         else:
             print("Такого этажа не существует")
-    
+
     def __len__(self):
         return self.number_of_floors
-    
+
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        else:
+            return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
 
