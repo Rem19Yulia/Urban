c42dcfe922642ef766621dca40262d0989db45a8
Rem19Yulia <kyv190591@gmail.com>
2024-09-15 17:35:33 +0300

    Update module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -5,6 +5,17 @@ second = 'Рамена мало было'
 result = list(map(lambda x, y: x == y, first, second))
 print("Результат сравнения:", result)
 
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + '\n')
+    return write_everything
+
+# Пример использования:
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
 class MysticBall:
     def __init__(self, words):
         self.words = words
