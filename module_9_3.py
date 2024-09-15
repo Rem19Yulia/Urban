f2aba48030901605ed0e7e675940b9aef72e7a4e
Rem19Yulia <kyv190591@gmail.com>
2024-09-15 18:05:49 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,7 +1,10 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
+first_result = (abs(len(a) - len(b)) for a,b in zip(first, second) if len(a) != len(b))
 
-second_result = (len(first[i]) - len(second[i]) for i in range(len(first)) if i < len(second))
+second_result = (len(first[i])! = len(second[i]) for i in range(len(first))) 
+
+print(list(first_result))
+print(list(second_result)) 
 
